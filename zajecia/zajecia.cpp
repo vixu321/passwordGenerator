fdf4b0d512d45679185b26ce750b2f86bffae687
vixu321 <vixu19@gmail.com>
2020-10-09 09:12:27 +0200

    Added more functionality: file generator, user input

--- zajecia/zajecia.cpp
+++ zajecia/zajecia.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
+#include <fstream>
 
 
 using namespace std;
 
-float number;
 
 int main()
 {
+    ofstream plikTekstowy("passwords.txt");
     char litera;
     int ilosc;
+    int dlugosc;
+    float number;
+
+    
     srand(time(0));
-    ilosc = 10;
+    cout << "Okresc dlugosc hasla: ";
+    cin >> dlugosc;
+    cout << "Ile takich hasel chcesz: ";
+    cin >> ilosc;
+
+
     for (int i = 0; i < ilosc; i++) {
-        number = rand() % 93 + 32;
-        litera = char(number);
-        cout << litera;
+        for (int i = 0; i < dlugosc; i++) {
+            number = rand() % 93 + 32;
+            litera = char(number);
+            plikTekstowy << litera;
+        }
+        plikTekstowy << '\n';
     }
+
+    plikTekstowy.close();
     return 0;
 }
 
