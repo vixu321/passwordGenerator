791e62c3fe7e5a0dc284c76621131fdf338becb1
vixu321 <vixu19@gmail.com>
2020-10-13 10:13:12 +0200

    More functionality, number generator, menu

--- zajecia/zajecia.cpp
+++ zajecia/zajecia.cpp
@@ -6,33 +6,83 @@
 
 using namespace std;
 
+void numberGenerator() {
+    int min, max;
+    int ilosc, dlugosc;
+    char litera;
+    float number;
+
+    system("CLS");
+
+    cout << "Podaj minimalna liczbe, ktora chcesz wylosowac: ";
+    cin >> min;
+    cout << "Podaj maksymalna liczbe, ktora chcesz wylosowac: ";
+    cin >> max;
+
+
+    srand(time(0));
+
+        number = rand() % (max - min + 1) + min;
+        cout << number << endl;
+        
+}
+
+void passwordGenerator() {
+
+
+   ofstream plikTekstowy("passwords.txt");
+   char litera;
+   int ilosc;
+   int dlugosc;
+   float number;
+
+   system("CLS");
+   srand(time(0));
+   cout << "Okresl dlugosc hasla: ";
+   cin >> dlugosc;
+   cout << "Ile takich hasel chcesz: ";
+   cin >> ilosc;
+
+
+   for (int i = 0; i < ilosc; i++) {
+       for (int i = 0; i < dlugosc; i++) {
+           number = rand() % 93 + 32;
+           litera = char(number);
+           plikTekstowy << litera;
+       }
+       plikTekstowy << '\n';
+   }
+   cout << "Hasla te zostaly zapisane w folderze projektu";
+
+   plikTekstowy.close();
+}
+
+
+
+
 
 int main()
 {
-    ofstream plikTekstowy("passwords.txt");
-    char litera;
-    int ilosc;
-    int dlugosc;
-    float number;
+    int wybor;
+    cout << "Co chcialbys wygenerowac: "<<endl;
+    cout << "1. Losowy numer w podanym zakresie." << endl;
+    cout << "2. Losowe haslo i zapis tego hasla do pliku" << endl;
+    cin >> wybor;
 
-    
-    srand(time(0));
-    cout << "Okresc dlugosc hasla: ";
-    cin >> dlugosc;
-    cout << "Ile takich hasel chcesz: ";
-    cin >> ilosc;
-
-
-    for (int i = 0; i < ilosc; i++) {
-        for (int i = 0; i < dlugosc; i++) {
-            number = rand() % 93 + 32;
-            litera = char(number);
-            plikTekstowy << litera;
-        }
-        plikTekstowy << '\n';
+    switch (wybor) {
+    case 1: numberGenerator();
+        break;
+    case 2: passwordGenerator();
+        break;
+    default: return 0;
     }
-
-    plikTekstowy.close();
     return 0;
 }
+
+
+
+ 
+
+
+
 
